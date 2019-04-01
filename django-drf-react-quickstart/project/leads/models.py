90ce388a449fbdea3f8c9e4d915d30758ce085aa
Josh Wood <wood2jp2@gmail.com>
2019-03-31 22:46:00 -0400

    added testing and another model

--- django-drf-react-quickstart/project/leads/models.py
+++ django-drf-react-quickstart/project/leads/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+# The models.Model is the object that Lead is inheriting from
+class Lead(models.Model):
+    name = models.CharField(max_length = 100)
+    email = models.EmailField()
+    message = models.CharField(max_length = 300)
+    phone = models.CharField(max_length = 10)
+    createdAt = models.DateTimeField(auto_now_add = True)
+
+
+class Person(models.Model):
+    name = models.CharField(max_length = 100)
+    cellPhone = models.CharField(max_length = 10)
